88301095ba1448cb21e858697e9752eeabc760fd
Hamza <hamzaraheem06@gmail.com>
2025-01-23 19:37:12 +0500

    add comments to todo list

--- 03_console_projects/todo_list.py
+++ 03_console_projects/todo_list.py
@@ -6,18 +6,18 @@ def add_task(todo_tasks):
         "isCompleted": False,
         "id": None
     } # defining a todo structure
-    task["task"] = input("Write a todo : ")
-    task["id"] = r.randrange(1000, 5656) // r.randrange(1, 50) # initializing the todo task
+    task["task"] = input("Write a todo : ") # initializing the todo task 
+    task["id"] = r.randrange(1000, 5656) // r.randrange(1, 50) # generating a random id to unique tracking of a task
 
     todo_tasks.append(task) # adding the todo to the list
-    print(f"'{task['task']}' has been added to todo list.")
+    print(f"'{task['task']}' has been added to todo list.") 
 
 
 def remove_todo(todo_list):
-    if not len(todo_list):
+    if not len(todo_list): # handling empty list case.
         print("The todo list is empty. No available task to be removed.")
-        return
-    display_todos(todo_list)
+        return 
+    display_todos(todo_list) # displaying all the task to assist user
     remove_id = int(input("Enter the id of the task to be removed: ")) # knowing which one to evict
     # todo_list = [task for task in todo_list if task["id"] != remove_id] # hunting the mother fucker down
     new_list = [] # creating a new list for the updation
@@ -26,9 +26,9 @@ def remove_todo(todo_list):
         if i["id"] == remove_id:
             print(f"'{i["task"]}' has been removed")
             continue
-        new_list.append(i)
+        new_list.append(i) # appending all the items except the one whose id matches.
     
-    if len(todo_list) == len(new_list):
+    if len(todo_list) == len(new_list): # handling 404 case.
         print(f"No such task with id {remove_id} exist.")
 
     return new_list # returning the HAS RIGHTS list
@@ -39,14 +39,14 @@ def remove_todo(todo_list):
 
 
 def check_todo(todo_list):
-    if not len(todo_list):
+    if not len(todo_list): # handling empty list case.
         print("Empty todo list. Cannot perform this operation")
         return
 
     display_todos(todo_list)
     complete_id = int(input("Enter the id of the completed task: ")) # knowing which one to tick
     for i in todo_list:
-        if i["id"] == complete_id:
+        if i["id"] == complete_id: # getting the complete task and checking if it already completed or not, otherwise mark it as complete
             if i['isCompleted']:
                 print(f"'{i["task"]}' has already been marked as completed")
                 return
@@ -54,27 +54,27 @@ def check_todo(todo_list):
             print(f"'{i["task"]}' has been marked as completed")
             return
         
-    print(f"No such task with id {complete_id} exist.")
+    print(f"No such task with id {complete_id} exist.") # handling 404 case
 
 
 def archive_task(todo_list: list, archived_list: list):
-    if not len(todo_list):
+    if not len(todo_list): # handling empty list case
         print("Empty todo list. Cannot perform this operation")
         return
 
-    display_todos(todo_list)
+    display_todos(todo_list) # displaying the list to assit the user for selection
     complete_id = int(input("Enter the id of the task: ")) # knowing which one to tick
     for i in todo_list:
         if i["id"] == complete_id:
-            archived_list.append(i)
+            archived_list.append(i) # using apped because it is a single item, and not a sequence.
             print(f"'{i["task"]}' has been moved to archive.")
-            todo_list.remove(i)
-            return todo_list, archived_list
+            todo_list.remove(i) # removing from the main list
+            return todo_list, archived_list # returning the updated list
         
-    print(f"No such task with id {complete_id} exist.")
+    print(f"No such task with id {complete_id} exist.") # handling 404 case
 
 def mark_all(todo_list):
-    if not len(todo_list) :
+    if not len(todo_list): # handling the empty list case
         print("Empty todo list. Cannot perform this operation")
         return
     
@@ -94,24 +94,24 @@ def mark_all(todo_list):
 
 
 def change_todo(todo_list):
-    if not len(todo_list) :
+    if not len(todo_list): # handling emtpy list case
         print("Empty todo list. Cannot perform this operation")
         return
     
-    display_todos(todo_list)
+    display_todos(todo_list) # displaying the list to assist user
     change_id = int(input("Enter the id of the task to be changed: "))
 
     for i in todo_list:
-        if i["id"] == change_id:
+        if i["id"] == change_id: 
             i["task"] = input("Enter the changes: ") # getting the changed task
             i["isCompleted"] = False # since the task has been changed, we check it as incomplete
             print("Changes have been saved.")
             return 
 
-    print(f"No such task with id {change_id} exist.")
+    print(f"No such task with id {change_id} exist.") # handling the 404 case
 
 def display_todos(todo_list):
-    if not len(todo_list) :
+    if not len(todo_list): # handling the empty list case
         print("Empty todo list.")
         return
 
@@ -124,10 +124,10 @@ def display_todos(todo_list):
 
 
 def remove_completed_todos(todo_list):
-    return [task for task in todo_list if not task["isCompleted"]]
+    return [task for task in todo_list if not task["isCompleted"]] # self explanatory
 
 def archive_complete(todo_list):
-    return [task for task in todo_list if task["isCompleted"]]
+    return [task for task in todo_list if task["isCompleted"]] # self explanatory
 
 
 # todo: https://chatgpt.com/c/678e22da-53cc-800a-a1ad-d4eadb1ad6cb
